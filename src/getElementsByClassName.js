edad7b0b41a8e873fe3d2d193d76e20914a09960
Eric Nague <ennague@gmail.com>
2018-04-12 00:01:20 -0700

    First commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className,node) {
+	//create a convenience array literal for results
+	var results= [];
+	// if has a specified node, use that. Otherwise, use the whole document as reference
+	node= node || document.body;
+
+	//search the live list and see if any nodes contain the className
+	if (node.classList) {
+		if(node.classList.contains(className)) {
+			//if they contain the className, push into results array
+			results.push(node)
+		}
+	}
+	//check to see if children nodes have the className
+	// iterate through the child nodes using a for loop for total number of children nodes
+	for (var i=0; i < node.children.length; i++) {
+		//store value if it contains className for the children node
+		var child= getElementsByClassName(className,node.children[i])
+		//use concat to individually add each child node as for loop iterates
+		results= results.concat(child)
+	}
+	//return results
+	return results;
+}
